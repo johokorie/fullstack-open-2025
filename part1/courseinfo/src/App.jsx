f0b24137176927315ece5e7c948ce4f9cac27cd0
John Okorie <ameachijohn001@gmail.com>
2025-01-20 16:06:25 +0100

    Submission: Exercise 1.2 Part 1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,11 +5,17 @@ const Header = (props) => {
 }
 const Content = (props) => {
   return (
-    <>
-      <p>{props.part1} {props.exercises1} </p>
-      <p>{props.part2} {props.exercises2} </p>
-      <p>{props.part3} {props.exercises3} </p>
-    </>
+    <div>
+      <Part parts={props.part1} exercises={props.exercises1}/>
+      <Part parts={props.part2} exercises={props.exercises2}/>
+      <Part parts={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.parts} {props.exercises}</p>
   )
 }
 const Total = (props) => {
