60c22de88d6adb93ec74175ed4073fb616c2ccd2
John Okorie <ameachijohn001@gmail.com>
2025-01-24 13:48:02 +0100

    Submission: Exercise 2.11 Part 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('');
 
+	// Data fetching
+	useEffect(() => {
+		axios.get("http://localhost:3001/persons")
+			.then((response) => { setPersons(response.data) })
+		},[])
 
 
 const filteredPersons = persons.filter((person) =>
@@ -49,7 +50,6 @@ const filteredPersons = persons.filter((person) =>
 			} else if (newNumber.trim() === "") {
 				alert("The number field can not be empty");
 			}
-			//  return emptyFieldAlert;
 		} else {
 			setPersons(persons.concat(newPersons));
 			setNewName("");
