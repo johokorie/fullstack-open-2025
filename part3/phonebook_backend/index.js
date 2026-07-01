9a4204a4d414e8b31be9ee7c440535e2ceb65898
John Okorie <ameachijohn001@gmail.com>
2026-07-01 18:22:16 +0100

    Submission: Exercise 3.3 part3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -40,6 +40,17 @@ app.get("/api/persons", (req, res) => {
 	res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+	const id = req.params.id
+	person = persons.find(person => (person.id === id))
+
+	if (person) {
+		res.json(person)
+	} else {
+		res.status(404).end()
+	}
+})
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
